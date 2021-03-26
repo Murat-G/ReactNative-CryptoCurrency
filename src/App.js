d6876cd2639f4c53eec50fd7ea1a5df5742373e8
MuratG <muratgozderesi34@gmail.com>
2021-03-26 23:15:23 +0300

    added tab navigator icon

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './pages/Home';
 import DetailCoins from './pages/DetailCoins';
 import Exchange from './pages/Exchange';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -23,9 +24,23 @@ function HomeComponent() {
 function App() {
     return (
         <NavigationContainer>
-            <Tab.Navigator initialRouteName="Home">
-                <Tab.Screen name="Home" component={HomeComponent} />
-                <Tab.Screen name="ExchangeComponentPage" component={Exchange} />
+            <Tab.Navigator initialRouteName="Home" tabBarOptions={{ activeTintColor: '#e91e63' }}>
+                <Tab.Screen name="Home" component={HomeComponent} options={{
+                    tabBarLabel: 'Coins',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="table-large" color={color} size={size} />
+                    ),
+                }} />
+
+                <Tab.Screen name="ExchangeComponentPage" component={Exchange}
+                    options={{
+                        tabBarLabel: 'Exchange Platforms',
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons name="format-columns" color={color} size={size} />
+                        ),
+                    }}
+                />
+
             </Tab.Navigator>
         </NavigationContainer>
     );
